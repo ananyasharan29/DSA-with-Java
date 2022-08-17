2badc57e4399bcf6996cf47a844148a91e3ecf72
Ananya Sharan <111418277+ananyasharan29@users.noreply.github.com>
2022-08-17 20:56:34 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,11 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+
+        float pi = 3.14f;
+        float r = 2.5f;
+
+        float A = pi * r * r;
+        System.out.println("Area of circle: " +A);
+
+    }
+}
